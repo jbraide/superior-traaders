3a138f77010c76707b239ff2689a3c50db9b3f95
Joseph <braidej@gmail.com>
2020-06-30 18:58:50 +0100

    added to localhost

--- superiortraders/superiortraders/settings.py
+++ superiortraders/superiortraders/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '6o0$1_k)3z4eybj62#feg9k%w)iog!lia9e_otwkp@cobvr#by'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['superior-traders.com', '161.35.67.201', 'localhost']
 
 
 # Application definition
